b9e9979be32db5b9de86c94226727b7f630d15a5
Evangelos Lamprou <vagos@lamprou.xyz>
2025-08-13 13:52:42 +0300

    Add Log instead of clog
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- src/script/LuaHelpers.cpp
+++ src/script/LuaHelpers.cpp
@@ -1,11 +1,12 @@
 #include "script/LuaHelpers.hpp"
+#include "Helpers.hpp"
 
 bool CheckLua(lua_State* L, int r)
 {
     if (r != LUA_OK)
     {
         std::string errormsg = lua_tostring(L, -1);
-        std::cout << errormsg << std::endl;
+        Log(errormsg);
         throw std::runtime_error("Lua error: " + errormsg);
     }
     return true;
