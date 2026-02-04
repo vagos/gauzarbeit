c3b56fc03f432f7785b9b801bd49c89a732952ab
Evangelos Lamprou <vagos@lamprou.xyz>
2026-02-04 00:47:48 -0500

    src: add JS init
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- scripts/Init.js
+++ scripts/Init.js
@@ -1,1 +1,16 @@
-print("JavaScript initialised!")
+if (typeof Gauzarbeit === "undefined") {
+  Gauzarbeit = {};
+}
+
+Gauzarbeit.Stats = {};
+
+Gauzarbeit.CreateStat = function (name, fn) {
+  Gauzarbeit.Stats[name] = fn;
+};
+
+Gauzarbeit.CreateStat("Attack", function (val) { return String(val); });
+Gauzarbeit.CreateStat("Defense", function (val) { return String(val); });
+Gauzarbeit.CreateStat("MaxHealth", function (val) { return String(val); });
+Gauzarbeit.CreateStat("CurrentHealth", function (val) { return String(val); });
+
+print("JavaScript initialised!");
