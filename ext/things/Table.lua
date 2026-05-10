dd77c4f8fe2c9cfb181fa905fd78f3f15519aed2
Evangelos Lamprou <vagos@lamprou.xyz>
2026-05-09 20:13:46 -0400

    Add Table
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

---
+++ ext/things/Table.lua
@@ -0,0 +1,42 @@
+function Table:doInit()
+
+end
+
+function Table:doDatabaseSave()
+    local items = self:getThings()
+    local saved = { tostring(#items) }
+
+    for _, thing in ipairs(items) do
+        table.insert(saved, thing:getName())
+    end
+
+    return table.concat(saved, "\n") .. "\n"
+end
+
+function Table:doDatabaseLoad()
+    local count = tonumber(Gauzarbeit.GetDBLine())
+    if not count then
+        return
+    end
+
+    for _, thing in ipairs(self:getThings()) do
+        self:loseItem(thing)
+    end
+
+    for _ = 1, count do
+        local item_name = Gauzarbeit.GetDBLine()
+        if not item_name or item_name == "" then
+            break
+        end
+
+        self:gainItem(item_name)
+    end
+end
+
+function Table:onInspect(inspector)
+    local contentList = "Contents:\n"
+    for _, thing in ipairs(self:getThings()) do
+        contentList = contentList .. " - " .. thing:getType() .. "\n"
+    end
+    return "A sturdy table that can hold various items.\n" .. contentList
+end
