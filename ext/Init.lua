8e8559b07be8036b5a8679b07c615ab590489fd8
Evangelos Lamprou <vagos@lamprou.xyz>
2026-05-10 11:04:53 -0400

    Add table to testing room
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- ext/Init.lua
+++ ext/Init.lua
@@ -21,6 +21,8 @@ Gauzarbeit.Spawn(0, 0, "Gun")
 Gauzarbeit.Spawn(0, 0, "Map")
 Gauzarbeit.Spawn(0, 0, "Compass")
 Gauzarbeit.Spawn(0, 0, "Cheesecake")
+local table = Gauzarbeit.Spawn(0, 0, "Table")
+table:gainItem(Gauzarbeit.Spawn(0, 0, "Cheesecake"))
 
 Gauzarbeit.Spawn(0, 0, "WelcomeMan")
 Gauzarbeit.Spawn(0, 0, "WantedBadge")
