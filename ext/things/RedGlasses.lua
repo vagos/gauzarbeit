b6f5c569a30ed2f491dfd20febc6f3aca02e3788
Evangelos Lamprou <vagos@lamprou.xyz>
2026-06-05 01:09:41 -0400

    Make RedGlasses use the Stat interface
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- ext/things/RedGlasses.lua
+++ ext/things/RedGlasses.lua
@@ -1,17 +1,22 @@
 
 function RedGlasses:onNotify(actor, notification_type, target)
 
-    if (notification_type == Gauzarbeit.Event.Inspect) then
-        
-        actor:sendMessage("\nYou see with your Red Glasses: \n\n")
+    if not target then
+        return
+    end
 
-        actor:sendMessage("HP: "..target:getHP().."\n")   
+    if (notification_type == Gauzarbeit.Event.Inspect) then
+        actor:sendMessage("\nYou see with your Red Glasses: \n\n")
+        local health = target:getStat("Health")
+        local max_health = target:getStat("MaxHealth")
+        if health == 0 then
+            health = max_health
+        end
+        actor:sendMessage("HP: "..health.."/"..max_health.."\n")
     end
 
 end
 
 function RedGlasses:onInspect(inspector)
-    
     return "A pair of glasses that lets you see MORE.\n"
-
 end
